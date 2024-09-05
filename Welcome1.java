f4cc088bac2d5dee93d96711dd16b09296246c7b
Nicholas Sharpe <nicholas.sharpe@keyin.com>
2024-09-05 10:52:30 -0230

    first commit

---
+++ Welcome1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+public class Welcome1{
+    //
+    
+    public static void main(String[] args) {
+        int a,b;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer");
+        a = input.nextInt();
+
+        //float c = 2.5f;
+
+       System.out.println("Integer entered = " + a);
+       System.out.printf("Integer entered = %d ",a);
+
+    }
+    ///
+
+}
